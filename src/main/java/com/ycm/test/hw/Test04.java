921824996b74020349750821e191473f259c31a0
rik.yang <rik.yang@webpowerchina.com>
2024-03-29 11:26:57 +0800

    hw code2

--- src/main/java/com/ycm/test/hw/Test04.java
+++ src/main/java/com/ycm/test/hw/Test04.java
@@ -34,33 +34,68 @@ public class Test04 {
 
 
 
-    private static List<LineNode> nodeList = new ArrayList<>();
 
-    private static int[] colors = null;
 
-    private static Integer sum = 0;
+
+    public static int solution = 0;
+
+
+    public static int[] colors = null;
+
+    public static List<Line> lineList = new ArrayList<>();
 
     public static Integer test() {
-        return 0;
-    }
-
-    public static void color(Integer index) {
-
-    }
-
-
-
-
-
-    public static class LineNode {
-        public Integer fromIndex;
-
-        public Integer toIndex;
-
-        public LineNode (Integer fromIndex, Integer toIndex) {
-            this.fromIndex = fromIndex;
-            this.toIndex = toIndex;
+        Scanner scanner = new Scanner(System.in);
+        String line1 = scanner.nextLine();
+        String[] arrays1 = line1.split(" ");
+        int num1 = Integer.parseInt(arrays1[0]);
+        int num2 = Integer.parseInt(arrays1[1]);
+        for (int i = 0; i < num2; i++) {
+            String line = scanner.nextLine();
+            String[] array = line.split(" ");
+            lineList.add(new Line(Integer.parseInt(array[0]) - 1, Integer.parseInt(array[1]) - 1));
         }
+        colors = new int[num1];
+        colored(0, num1);
+        return solution;
+    }
+
+    private static void colored(int index, int number) {
+        if (index < number) {
+            if (colors[index] == 0) {
+                colors[index] = 1;
+                colored(index + 1, number);
+                colors[index] = 2;
+                for (Line line : lineList) {
+                    if (line.from == index) {
+                        colors[line.to] = 1;
+                    }
+                    if (line.to == index) {
+                        colors[line.from] = 1;
+                    }
+                }
+                colored(index + 1, number);
+            } else {
+                colored(index + 1, number);
+            }
+            // 回溯
+            colors[index] = 0;
+        } else {
+            solution++;
+        }
+    }
+
+
+    static class Line {
+        public int from;
+
+        public int to;
+
+        public Line(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
 
     }
 

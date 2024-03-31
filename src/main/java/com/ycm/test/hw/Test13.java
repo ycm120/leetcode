fe4030c443b6c2c21528412322c32ba00d6dd7c0
ycm <ycm12580@gmail.com>
2024-03-31 20:22:37 +0800

    hw code2

--- src/main/java/com/ycm/test/hw/Test13.java
+++ src/main/java/com/ycm/test/hw/Test13.java
@@ -27,12 +27,49 @@ public class Test13 {
      * 说明：可以等分的情况有： 4个子集（5），（1，4），（2，3），（2，3）2个子集（5，1，4），（2，3，2，3）但最小的为5。
      */
     public static void main(String[] args) {
-        String input="1 2 3 4 5 6 7";
-
+        String input="4 3 2 3 5 2 1";
+        String[] arrays = input.split(" ");
+        int length = arrays.length;
+        Integer[] numbers = new Integer[length];
+        int sum = 0;
+        int maxNum = 0;
+        for (int i = 0; i < length; i++) {
+            numbers[i] = Integer.parseInt(arrays[i]);
+            sum += numbers[i];
+            maxNum = Math.max(numbers[i], maxNum);
+        }
+        List<Integer> groupList = new ArrayList<>();
+        for (int i = length; i >= 2; i--) {
+            if (sum % i == 0 && sum / i >= maxNum) {
+                groupList.add(i);
+            }
+        }
+        Arrays.sort(numbers, (num1, num2) -> num2 - num1);
+        for (int i = 0; i < groupList.size(); i++) {
+            int groupNum = groupList.get(i);
+            if (canGroup(groupNum, numbers, sum)) {
+                System.out.println(sum / groupNum);
+                return;
+            }
+        }
+        System.out.println(" " + sum);
     }
     public static boolean canGroup(int groupNum, Integer[] arrayNum, int sum){
-
-
+        int target = sum / groupNum;
+        int[] groupCount = new int[groupNum];
+        for(int number : arrayNum) {
+            for (int i = 0; i < groupNum; i++) {
+                if(groupCount[i] + number <= target) {
+                    groupCount[i] += number;
+                    break;
+                }
+            }
+        }
+        for(int count : groupCount) {
+            if(target != count) {
+                return false;
+            }
+        }
         return true;
     }
 

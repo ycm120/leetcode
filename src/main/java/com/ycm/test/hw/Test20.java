f6fb04022f2366bb79175937fdad7468f098e35f
rik.yang <rik.yang@webpowerchina.com>
2024-04-01 15:34:33 +0800

    hw code2

--- src/main/java/com/ycm/test/hw/Test20.java
+++ src/main/java/com/ycm/test/hw/Test20.java
@@ -1,5 +1,9 @@
 package com.ycm.test.hw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  *
@@ -29,7 +33,55 @@ public class Test20 {
      * 4
      */
     public static void main(String[] args) {
+        Integer[] arrays = new Integer[]{4, 3, 2, 3, 5, 2, 1};
+        int length = arrays.length;
+        int sum = 0;
+        int maxNum = 0;
+        int maxGroup = 1;
+        for(int num : arrays) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        List<Integer> groupList = new ArrayList<>();
+        for(int i = length; i >= 2; i--) {
+            if (sum % i == 0 && maxNum <= sum / i) {
+                groupList.add(i);
+            }
+        }
+        Arrays.sort(arrays, (num1, num2) -> num2 - num1);
+        for(int groupNum : groupList) {
+            if (canGroup(groupNum, arrays, sum)) {
+                maxGroup = groupNum;
+                break;
+            }
+        }
+        System.out.println(maxGroup);
 
+    }
+
+
+
+
+
+
+
+    public static boolean canGroup(int groupNum, Integer[] arrays, int sum) {
+        int[] groupArrays = new int[groupNum];
+        int target = sum / groupNum;
+        for(int num : arrays) {
+            for(int i = 0; i < groupNum; i++) {
+                if (groupArrays[i] + num <= target) {
+                    groupArrays[i] += num;
+                    break;
+                }
+            }
+        }
+        for(int num : groupArrays) {
+            if(num != target) {
+                return false;
+            }
+        }
+        return true;
     }
 
 

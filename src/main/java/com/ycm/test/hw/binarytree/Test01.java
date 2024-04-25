c11dd59f2b6a76ff8b6b8bdd2c5437e04c23ff61
rik.yang <rik.yang@webpowerchina.com>
2024-04-25 10:21:05 +0800

    hw code2

---
+++ src/main/java/com/ycm/test/hw/binarytree/Test01.java
@@ -0,0 +1,86 @@
+package com.ycm.test.hw.binarytree;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 二叉树的中序遍历、二叉搜索树的第k个节点
+ * 方法一：递归
+ * 思路与算法
+ * 首先我们需要了解什么是二叉树的中序遍历：按照访问左子树——根节点——右子树的方式遍历这棵树，而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树。因此整个遍历过程天然具有递归的性质，我们可以直接用递归函数来模拟这一过程。
+ * 定义 inorder(root) 表示当前遍历到 root 节点的答案，那么按照定义，我们只要递归调用 inorder(root.left) 来遍历 root 节点的左子树，
+ * 然后将 root 节点的值加入答案，再递归调用inorder(root.right) 来遍历 root节点的右子树即可，递归终止的条件为碰到空节点。
+ * 方法二：迭代
+ * 思路与算法
+ * 方法一的递归函数我们也可以用迭代的方式实现，两种方式是等价的，区别在于递归的时候隐式地维护了一个栈，
+ * 而我们在迭代的时候需要显式地将这个栈模拟出来，其他都相同，具体实现可以看下面的代码。
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/solutions/412886/er-cha-shu-de-zhong-xu-bian-li-by-leetcode-solutio/
+ *
+ * @author: rik.yang
+ * @date: 2024/4/25 09:56
+ */
+public class Test01 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> integers = inorderTraversal1(null);
+        System.out.println(JSON.toJSONString(integers));
+    }
+
+    public static List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private static void inorder(TreeNode node, List<Integer> list) {
+        if(null == node) {
+            return;
+        }
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+
+
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while(root != null || !stack.isEmpty()) {
+            while(root.left != null) {
+                stack.push(root);
+                stack.push(root.left);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+
+
+}
